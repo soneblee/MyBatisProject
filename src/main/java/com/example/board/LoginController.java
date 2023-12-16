3e3122ed7e537f1152fea6689302a81b053f4e40
soneblee <unbanyyy@handong.ac.kr>
2023-12-16 20:27:10 +0900

    update file

--- src/main/java/com/example/board/LoginController.java
+++ src/main/java/com/example/board/LoginController.java
@@ -1,4 +1,45 @@
 package com.example.board;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+//@RequestMapping(value="/login")
 public class LoginController {
+    @Autowired
+    UserServiceImpl service;
+    @RequestMapping(value = "/login/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+    @RequestMapping(value = "/login/loginOk", method = RequestMethod.POST)
+    public String loginCheck(HttpSession session, UserVO vo) {
+        String returnURL = "";
+        if (session.getAttribute("login") != null) {
+            session.removeAttribute("login");
+        }
+
+        UserVO loginvo = service.getUser(vo);
+        if (loginvo != null) {
+
+            System.out.println("로그인 성공!");
+            session.setAttribute("login", loginvo);
+            returnURL = "redirect:/board/list";
+
+        } else {
+            System.out.println("로그인 실패!");
+            returnURL = "redirect:/login/login";
+        }
+        return returnURL;
+    }
+
+    @RequestMapping(value = "/login/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/login/login";
+    }
 }
--- src/main/java/com/example/board/UserDAO.java
+++ src/main/java/com/example/board/UserDAO.java
@@ -1,4 +1,18 @@
 package com.example.board;
 
+
+import com.example.board.UserVO;
+import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class UserDAO {
+
+    @Autowired
+    SqlSessionTemplate sqlSession;
+    public UserVO getUser(UserVO vo) {
+        return sqlSession.selectOne("User.getUser", vo);
+    }
 }
